472d1f8832ca513847ddab2d53826f50a1574b68
Keberson <k3berson@gmail.com>
2023-07-21 15:23:41 +0300

    (task 2) edit base class

--- src/api.py
+++ src/api.py
@@ -1,7 +1,19 @@
-class ApiRequest:
-    def __init__(self, req, payload):
-        self.req = req
-        self.payload = payload
+from abc import ABC, abstractmethod
+
+
+class ApiRequest(ABC):
+    def __init__(self, payload):
+        self.__payload = payload
+
+    TYPE = 'None'
+
+    @abstractmethod
+    def change_payload(self, payload):
+        pass
+
+
+class GetApiRequest(ApiRequest):
+    TYPE = 'GET'
 
     def change_payload(self, payload):
-        self.payload = payload
+        self.__payload = payload
