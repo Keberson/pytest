c8c24ec16d2c10ae6ac08285086578020e23d1de
Keberson <k3berson@gmail.com>
2023-08-07 16:26:26 +0300

    добавлено покрытие тестами put + добавлены failed тесты

--- test/test_petstore.py
+++ test/test_petstore.py
@@ -127,8 +127,55 @@ class TestPet:
         assert response.status_code == 405
 
     @pytest.mark.edit
-    def test_put(self):
-        pass
+    def test_put_correct(self, pets, headers):
+        for i in range(len(pets)):
+            pet = pets[i]
+            pet.name = 'Vasya'
+            serialized = json.dumps(pet, cls=PetEncoder)
+
+            response = requests.put(ENDPOINT, data=serialized, headers=headers)
+
+            assert response.status_code == 200
+
+            try:
+                got_pet = Pet(**json.loads(response.content))
+                assert pet == got_pet
+
+                pets[i] = pet
+            except ValidationError:
+                assert False
+
+    @pytest.mark.parametrize(
+        'payload',
+        [
+            {
+                 'id': "0",
+                 'name': "new doggie",
+                 'photoUrls': ["url"],
+                 'category': {'id': 0, 'name': "string"},
+                 'tags': [{'id': 0, "name": "string"}],
+                 'status': "available"
+             },
+        ]
+    )
+    @pytest.mark.edit
+    def test_put_incorrect_id(self, headers, payload):
+        response = requests.put(ENDPOINT, data=payload, headers=headers)
+
+        assert response.status_code == 400
+
+    @pytest.mark.edit
+    def test_put_incorrect_validation(self, pets, headers):
+        for pet in pets:
+            payload = {'id': pet.id, 'имя': 'Максим', 'фамилия': 'есть'}
+
+            response = requests.post(
+                ENDPOINT,
+                data=payload,
+                headers=headers
+            )
+
+            assert response.status_code == 405
 
     @pytest.mark.parametrize(
         'status',
@@ -136,6 +183,7 @@ class TestPet:
     )
     def test_get_find_by_status_correct(self, status):
         params = {'status': status}
+
         response = requests.get(f'{ENDPOINT}/findByStatus', params=params)
 
         assert response.status_code == 200
@@ -146,13 +194,26 @@ class TestPet:
             except ValidationError:
                 assert False
 
+    @pytest.mark.parametrize(
+        'status',
+        ['-', 'куплен', 'продан', 'реклама недорого']
+    )
+    def test_get_find_by_status_incorrect(self, status):
+        params = {'status': status}
+
+        response = requests.get(f'{ENDPOINT}/findByStatus', params=params)
+
+        assert response.status_code == 400
+
     def test_get_id_correct(self, pets):
         for pet in pets:
             response = requests.get(f'{ENDPOINT}/{pet.id}')
+
             assert response.status_code == 200
 
             try:
                 got_pet = Pet(**json.loads(response.content))
+
                 assert pet == got_pet
             except ValidationError:
                 assert False
@@ -163,6 +224,7 @@ class TestPet:
     )
     def test_get_id_incorrect(self, payload):
         response = requests.get(f'{ENDPOINT}/{payload}')
+
         assert response.status_code == 404
 
     def test_post_id_correct(self, pets):
@@ -170,6 +232,23 @@ class TestPet:
             params = {'name': 'Vasya', 'status': 'sold'}
             response = requests.post(f'{ENDPOINT}/{pet.id}', params=params)
             assert response.status_code == 200
+
+            params = {'name': 'Ne Vasya'}
+            response = requests.post(f'{ENDPOINT}/{pet.id}', params=params)
+            assert response.status_code == 200
+
+    @pytest.mark.parametrize(
+        'payload',
+        [
+            {'name': 123, 'status': 'sold'},
+            {'name': 'Vasya', 'status': 'продали васю('},
+        ]
+    )
+    def test_post_id_incorrect(self, pets, payload):
+        for pet in pets:
+            response = requests.post(f'{ENDPOINT}/{pet.id}', params=payload)
+
+            assert response.status_code == 405
 
     def test_delete_id_correct(self, pets):
         for pet in pets:
